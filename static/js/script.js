e173ad5c04c8d2c8447d82496b23191e29669160
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-17 10:11:27 +0800

    Removed tabs because security

--- static/js/script.js
+++ static/js/script.js
@@ -8,6 +8,17 @@ const bd = document.createElement('div');
 bd.setAttribute("id", "backdrop");
 document.body.appendChild(bd);
 
+class yolov4Header extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+    <h1>The YOLOV4 Fursuit Detector</h1>
+    `;
+  }
+}
+window.customElements.define('yolov4-header', yolov4Header);
+
+
 class yolov4Container extends HTMLElement {
   constructor() {
     super();
@@ -26,3 +37,5 @@ class yolov4ModelWarning extends HTMLElement {
   }
 }
 window.customElements.define('yolov4-model-warning', yolov4ModelWarning);
+
+
