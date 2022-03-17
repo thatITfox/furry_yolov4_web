ab83a08af393bb4ffb35c6df692c35e94e5f8b6a
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-17 15:04:51 +0800

    restored the upload tab lol

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def delete(session):
     os.remove(f'static/{session}.png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
