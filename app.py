88fcd7293e86563829369725341123ed74061ce4
thatITfox <maxthecomputerfox@gmail.com>
2022-03-17 07:49:59 +0700

    fixed security problem

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 import random
 import string
-
+import os
 
 def fitimginbrowser(img):
     h, w, _ = img.shape
@@ -28,19 +28,25 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # generate a random string as the name of the image and as a session token
+        session = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
         image = request.files['image']
         image = np.asarray(bytearray(image.read()), dtype="uint8")
         og_image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         image = cv2.resize(og_image, (512, 512))
         boxes = do_detect(model_pt, image, 0.5, 0.4, use_cuda=False)
         print(boxes[0])
-        plot_boxes_cv2(fitimginbrowser(og_image), boxes[0], 'static/result.png', class_names=load_class_names('obj.names'))
-        return redirect(url_for('results'))
+        plot_boxes_cv2(fitimginbrowser(og_image), boxes[0], f'static/{session}.png', class_names=load_class_names('obj.names'))
+        return redirect(f'/results/{session}')
     return render_template('index.html')
 
-@app.route('/results')
-def results():
-    return render_template('results.html')
+@app.route('/results/<session>')
+def results(session):
+    return render_template('results.html', session=session)
+
+@app.route('/delete/<session>')
+def delete(session):
+    os.remove(f'static/{session}.png')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
