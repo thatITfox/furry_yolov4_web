3506999136b118936fa57924d6be7dc1027cfb11
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-17 10:14:10 +0800

    Removed tabs because security (#10)

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 zip -F -o "yolov4-zip-model/yolov4-pytorch.zip" --out "yolov4-pytorch_out.zip"
 unzip "yolov4-pytorch_out.zip"
 rm -rf "yolov4-pytorch_out.zip"
 
-# python app.py
+python app.py
