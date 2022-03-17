7c8274ce10e4988a4c9119f70eb0ddbb966cb003
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-17 08:19:57 +0800

    Finally fixed bash script with the zip files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# pip install -r requirements.txt
+
+zip -F "yolov4-zip-model/yolov4-pytorch.zip" --out "yolov4-pytorch_out.zip"
+unzip "yolov4-pytorch_out.zip"
+rm -rf "yolov4-pytorch_out.zip"
+
+# python app.py
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-pip install -r 'requirements.txt'
-
-unzip "./yolov4-zip-model/yolov4-pytorch.zip"
-
-python app.py
