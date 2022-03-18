b352094a70330807b227c2a1f73023b6153bba89
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-18 21:05:18 +0800

    nope screw this
    refactoring stuff

--- static/script.js
+++ static/script.js
@@ -1,13 +1,9 @@
-/* this JS script allows it to be reusable in all pages without
- * rewriting repetitive HTML elements when we update something and
- * updating it to all of the pages and it saves the hassle
- */
-
 // adds the backdrop element
 const bd = document.createElement('div');
 bd.setAttribute("id", "backdrop");
 document.body.appendChild(bd);
 
+// CUSTOM ELEMENTS
 class yolov4Header extends HTMLElement {
   constructor() {
     super();
@@ -16,26 +12,21 @@ class yolov4Header extends HTMLElement {
     `;
   }
 }
-window.customElements.define('yolov4-header', yolov4Header);
 
+window.customElements.define('yolov4-header', yolov4Header);
 
 class yolov4Container extends HTMLElement {
   constructor() {
     super();
   }
 }
+
 window.customElements.define('yolov4-container', yolov4Container);
 
 class yolov4ModelWarning extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `
-    <p style="font-size: 90%;">
-      If there's something wrong or some inaccuracies or something
-    </p>
-    `;
   }
 }
+
 window.customElements.define('yolov4-model-warning', yolov4ModelWarning);
-
-
