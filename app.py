e918d1059f0f767ccf3ab40e4d199cf885e89613
thatITfox <maxthecomputerfox@gmail.com>
2022-03-21 12:55:11 +0700

    added count bbox feature

--- app.py
+++ app.py
@@ -9,14 +9,6 @@ import random
 import string
 import os
 
-def fitimginbrowser(img):
-    h, w, _ = img.shape
-    if h >= w:
-        w, h = int(w*500/h), 500
-    else:
-        w, h = 500, int(h*500/w)
-    return w, h
-
 # load weights and model from pytorch format and the config from darknet
 model_pt = darknet2pytorch.Darknet('yolov4-obj.cfg', inference=True)
 model_pt.load_state_dict(torch.load('yolov4-pytorch.pth'))
@@ -37,6 +29,8 @@ def index():
             image = cv2.resize(og_image, (512, 512))
             boxes = do_detect(model_pt, image, 0.5, 0.4, use_cuda=False)
             print(boxes[0])
+            with open(f'static/{session}.txt', 'w') as f:
+                f.write(str(len(boxes[0])) + '\n')
             plot_boxes_cv2(og_image, boxes[0], f'static/{session}.png', class_names=load_class_names('obj.names'))
             return redirect(f'/results/{session}' )
         except Exception as e:
@@ -46,9 +40,10 @@ def index():
 
 @app.route('/results/<session>')
 def results(session):
+    with open(f"static/{session}.txt", "r") as f:
+        num_boxes = int(f.readline())
     try:
-        width, height = fitimginbrowser(cv2.imread(f'static/{session}.png'))
-        return render_template('results.html', session=session, width=width, height=height)
+        return render_template('results.html', session=session, num_boxes=num_boxes)
     except:
         return redirect(url_for('index'))
 
@@ -56,6 +51,7 @@ def results(session):
 def delete(session):
     try:
         os.remove(f'static/{session}.png')
+        os.remove(f'static/{session}.txt')
     except:
         print(f'tried to remove {session}.png but it does not exist')
 
