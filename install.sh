0409ea6d1af04716e8ca78da2e1279b8147c0e56
thatITfox <maxthecomputerfox@gmail.com>
2022-03-22 19:33:37 +0700

    edited stuff for faster deployment

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# this script is used for debian based linux systems
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
+sudo apt install python3-opencv
+pip3 install torch==1.11.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
 
-zip -F -o "yolov4-zip-model/yolov4-pytorch.zip" --out "yolov4-pytorch_out.zip"
-unzip "yolov4-pytorch_out.zip"
-rm -rf "yolov4-pytorch_out.zip"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PpOzBo8yZzQZ8ncMdreCrejd_P23fYiG' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1PpOzBo8yZzQZ8ncMdreCrejd_P23fYiG" -O yolov4-pytorch.pth && rm -rf /tmp/cookies.txt
 
 python app.py
