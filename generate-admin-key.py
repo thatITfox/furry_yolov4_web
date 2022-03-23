1dd3d184ce295aaf3aa5906cb533b7c680ab6259
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-24 02:18:09 +0800

    Init basic Flask + React App

--- generate-admin-key.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-def random_ascii_char(length):
-    return ''.join(random.choices(string.ascii_letters, k=length))
-
-with open('admin-key', 'w') as key:
-    key.write(random_ascii_char(32))
